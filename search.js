bc51ac14c015033990216c2d3fa2ab446ee99556
jordan-creyelman <creyelmanjo@gmail.com>
2022-01-20 15:19:40 +0100

    finish

--- search.js
+++ search.js
@@ -1,42 +1,99 @@
+const submit = document.getElementById("searchButton");
+const userSearch = document.getElementById('searchInput');
+const selector = document.getElementById('movieSelector');
 
-const search = async (film)=>{  
-  var titre = 'http://www.omdbapi.com/?s='+film+'&apikey=ff461133'
-  const response=await fetch(titre);
-  const data = await response.json();
-  var i = 0;
-  var src= document.getElementById("card");
+const movieFinder = () => {
+  const url = `http://www.omdbapi.com/?s=${userSearch.value}&page=2&apikey=ff461133`
 
-  while(i<data.Search.length){
-    
-  src.innerHTML += `
-  <div id="movieCard-${i}" class="card my-2 movie-card">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="${data.Search[i].Poster}" class="card-img movie-img" alt="movie poster">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${data.Search[i].Title}</h5>
-        <p> ${data.Search[i].Year} </p>
-        <button id="readMore-${i}" class="btn btn-primary">Read more</button>
+  fetch(url, {
+    method: "POST",
+  })
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      selector.innerHTML = "";
+      movieInfo(data);
+    ;}))
+    .catch((error) => console.error('error:', error));
+  };
+
+//L'utilisateur peut chercher une liste de films/séries grâce à des mots clefs
+const movieInfo = (data) => {
+  const dataArray = data.Search;
+  dataArray.forEach( (item) => {
+    const image = item.Poster;
+    const title = item.Title;
+    const date = item.Year;
+    const id = item.imdbID;
+
+    showMovie(selector, image, title, date, id);
+  });
+};
+
+submit.addEventListener('click', () => {
+  movieFinder();
+});
+
+const showMovie = (selector, image, title, date, id) => {
+  selector.innerHTML += `
+  <div class="container text-center">
+    <div class="card mb-3 text-left" style="width: 500px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="${image}" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${date}</p>
+            <button class="buttonLearnMore btn btn-primary" onclick="showLearnMore('${id}')">Plus d'infos</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>`;
-i++;
+  `
+  
+}
+
+const showLearnMore =  async function (id) {
+  const urlById = await fetch(`http://www.omdbapi.com/?i=${id}&page=2&apikey=ff461133`)
+  const popUpSelector = document.getElementById('myPopup');
+  const popUpDetails = await urlById.json();
+  const image = popUpDetails.Poster;
+  const title = popUpDetails.Title;
+  const date = popUpDetails.Year;
+  const plot = popUpDetails.Plot;
+  
+  try {
+    popUpSelector.innerHTML = "";
+    modal(popUpSelector, image, title, date, plot);
+  } catch (error) {
+    console.error("error : ", error)
+  }
 };
+
+const modal = (selector, image, title, date, plot) => {
+  selector.innerHTML += `
+  <div class="container popup__container popupShit text-center">
+    <div class="card text-left" style="width: 500px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="${image}" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${date}</p>
+            <p>${plot}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
 }
 
 
 
-document.getElementById("searchButton").addEventListener("click",()=>{
-  
-  var film =document.getElementById("searchInput").value;
-  var src= document.getElementById("card");
-  src.innerHTML="";
-  search(film);
-  
-  
 
-});
 
